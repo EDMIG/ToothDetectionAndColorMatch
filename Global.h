7f22e435b22d5dee3dc015fe99a1e91260f7cf88
yangyangHu <1543541652@qq.com>
2017-08-08 22:24:22 +0800

    Create Global.h

---
+++ Global.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "cv.h"
+#include "highgui.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <vector>
+
+using namespace cv;
+using namespace std;
